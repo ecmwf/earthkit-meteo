b2695965a427c398c132c84f848284fe12f7f8a8
Martin Janousek <martin.janousek@ecmwf.int>
2024-05-24 15:34:04 +0200

    Implement quantile-type event classes. Cleaning of unused methods. Still not sure if the API is correct, pending the implementation of probability_quantiles methods.

--- earthkit/meteo/score/events.py
+++ earthkit/meteo/score/events.py
@@ -1,41 +1,8 @@
-
 import numpy as np
 import scipy
-from typing import Literal
+from typing import Literal, Union, Unpack
 
-QUANTILE_MAP = {
-    100: "percentile",
-    3: "tercile",
-    4: "quartile",
-    5: "quintile",
-    10: "decile",
-}
-QUANTILE_NAMES = list(QUANTILE_MAP.values())
-
-
-# MOVE THIS TO A SERIES OF CLASSES
-# def event_field(event, field, statistics=None, dim=None):
-#     # dim solely indicates which dimension is the ensemble when `field` is an ensemble
-#     # TODO: check if this actually works for "abs" when field has coordinates along `dim` dimension?
-#     bev = BinaryEvent(**event)
-#     if bev.requires_climatology and statistics is None:
-#         raise ValueError(
-#             "Binary event %s requires statistics/climatology but none is present" % bev
-#         )
-#     if bev["type"] == "abs":
-#         if dim is not None and dim in field.dims:
-#             threshold = xarray.full_like(field[{dim: 0}], bev["value"])
-#         else:
-#             threshold = xarray.full_like(field, bev["value"])
-#     elif bev["type"] == "stdev":
-#         threshold = bev["value"] * statistics["stdev"]
-#     else:
-#         threshold = bev.select_quantile_array(statistics["quantile"])
-#     if bev["is_anomaly"]:
-#         threshold = threshold + statistics["mean"]
-#     valid_mask = _common_valid_mask(field, threshold, dim=dim)
-#     return bev.operator()(field, threshold), valid_mask
-
+QUANTILE_TYPES = Literal["percentile","tercile"]
 
 operators = {
     ">": lambda x, y: x > y,
@@ -44,16 +11,29 @@ operators = {
     "<=": lambda x, y: x <= y,
 }
 
+# NB: ATM this implementation works as follows:
+#       event=SomeEvent(value=val,...,mean=array,stdev=array,quantiles={val:array})
+#                                     ^^^^ **statistics ^^^^^^^^^^^^^^^^^^^^^^^^^^
+#       bs = brier_gaussian(event,obs_array)
+#     an alternative would be:
+#       event=SomeEvent(value=val,...)
+#       bs = brier_gaussian(event,obs_array,mean=array,stdev=array,quantiles={val:array})
+#                                           ^^^^ **statistics ^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-class Event(dict):
-    """A binary event. It describes the event's type, value, operator and whether it is
-    anomaly-based or not.
+
+
+class _Event:
+    """Binary event.
 
     Args:
         operator (str): Threshold operator, one of (">", ">=", "<", "<=").
-        value (float, int): Threshold value.
-        type (str): Threshold value type, one of ("abs", "stdev", "percentile", "tercile").
+        value (float, int, str): Threshold value.
+        type (str): Threshold value type, one of ("abs", "stdev", QUANTILE_TYPES).
         is_anomaly (bool): Whether the threshold value is an anomaly or not.
+        statistics (dict): Auxiliary arrays of statistics:
+            * mean (ndarray)
+            * stdev (ndarray)
+            * quantiles (dict of ndarray)
 
     Examples:
 
@@ -64,28 +44,30 @@ class Event(dict):
         * geopotential anomaly greater than 1.5*standard deviation:
             BinaryEvent(operator=">",value=1.5,type="stdev",is_anomaly=True)
         * precipitation greater than 90-th percentile:
-            BinaryEvent(operator=">",value=90,type="percentile",is_anomaly=False)
+            BinaryEvent(operator=">",value="90:100",type="quantiles",is_anomaly=False)
 
     """
 
     def __init__(
         self,
-        operator:Literal[">", ">=", "<", "<="],
-        value:float,
-        type:Literal["abs", "stdev", "percentile", "tercile"],
-        is_anomaly:bool,
-        **statistics,
+        operator: Literal[">", ">=", "<", "<="],
+        value: Union[float, int, str],
+        type: Literal[Literal["abs", "stdev"],QUANTILE_TYPES],   # this is probably illegal
+        is_anomaly: bool,
+        **statistics: Unpack[Union[np.ndarray, dict]],
     ):
-        
+        self._operator = operator
+        self._value = value
+        self._type = type
+        self._is_anomaly = is_anomaly
+        self._statistics = statistics
+
         self.greater_event = operator in (">", ">=")
 
-        super().__init__(
-            operator=operator, value=value, type=type, is_anomaly=is_anomaly, **statistics
-        )
-
     def __str__(self):
+        # TODO: implement in subclasses
         def ord_str(n):
-            """generate and ordinal string from an integer number"""
+            """generate an ordinal string from an integer number"""
             dd = {1: "st", 2: "nd", 3: "rd"}
             nn = int(n) % 10
             try:
@@ -93,51 +75,34 @@ class Event(dict):
             except KeyError:
                 return "{:d}th".format(int(n))
 
-        if self["type"] in QUANTILE_NAMES:
-            val_str = ord_str(int(self["value"] + 0.5))
+        if self._type in QUANTILE_TYPES:
+            val_str = ord_str(int(self._value + 0.5))
         else:
-            val_str = str(self["value"])
-        if self["is_anomaly"]:
+            val_str = str(self._value)
+        if self._is_anomaly:
             name = "anomaly"
         else:
             name = "field"
-
-        name += self["operator"] + val_str
-        if self["type"] != "abs":
-            name += " " + self["type"]
+        name += self._operator + val_str
+        if self._type != "abs":
+            name += " " + self._type
         return name
 
     def operator(self):
-        return operators[self["operator"]]
+        return operators[self._operator]
 
-    @property
-    def requires_climatology(self):
-        return self["is_anomaly"] or self["type"] != "abs"
+    def _threshold_array(self):
+        raise NotImplementedError
 
-    def select_quantile_array(
-        self, clim_ds, quantile=None, quantile_dim="quantile_"
-    ):
-        if quantile is None:
-            quantile = self["value"]
-        name_in_clim_ds = self["type"]
-        return clim_ds.loc[
-            {quantile_dim: "%d:%d" % (quantile, QUANTILE_MAP[name_in_clim_ds])}
-        ].drop_vars(quantile_dim)
-
-
-    def threshold_array(self):
+    def _gaussian_pdf_value(self):
         raise NotImplementedError
 
     def event_array(self, array):
-        """Event occurrence in the array.
-        """
-        return self.operator()(array, self.threshold_array()).astype(float)
-
-    def _gaussian_threshold(self):
-        raise NotImplementedError
+        """Event occurrence in the array."""
+        return self.operator()(array, self._threshold_array()).astype(float)
 
     def probability_gaussian(self):
-        threshold = self._gaussian_threshold()
+        threshold = self._gaussian_pdf_value()
         with np.errstate(invalid="ignore"):
             prob = scipy.stats.norm().cdf(threshold)
         if self.greater_event:
@@ -145,69 +110,127 @@ class Event(dict):
         return prob
 
 
-class AbsEvent(Event):
+class AbsEvent(_Event):
 
-    def __init__(self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics):
-        super().__init__(value=value, operator=operator, type="abs", is_anomaly=False, **statistics)
+    def __init__(
+        self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics
+    ):
+        super().__init__(
+            value=value, operator=operator, type="abs", is_anomaly=False, **statistics
+        )
 
-    def threshold_array(self):
-        return self["value"]
+    def _threshold_array(self):
+        return self._value
 
-    def _gaussian_threshold(self):
-        return (self["value"] - self["mean"]) / self["stdev"]
+    def _gaussian_pdf_value(self):
+        return (self._value - self._statistics["mean"]) / self._statistics["stdev"]
 
 
 class AbsAnomalyEvent(AbsEvent):
 
-    def __init__(self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics):
+    def __init__(
+        self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics
+    ):
         # NB: the granny Event has still is_anomaly=False!
         super().__init__(value=value, operator=operator, **statistics)
 
-    def threshold_array(self):
-        return self["value"] + self["mean"]
+    def _threshold_array(self):
+        return self._value + self._statistics["mean"]
 
-    def _gaussian_threshold(self):
-        return self["value"] / self["stdev"]
+    def _gaussian_pdf_value(self):
+        return self._value / self._statistics["stdev"]
 
 
-class StdevEvent(Event):
+class StdevEvent(_Event):
+    # NB: this is a weird case, TODO: check with ML if it makes sense at all
 
-    def __init__(self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics):
-        super().__init__(value=value, operator=operator, type="stdev", is_anomaly=False, **statistics)
+    def __init__(
+        self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics
+    ):
+        super().__init__(
+            value=value, operator=operator, type="stdev", is_anomaly=False, **statistics
+        )
 
-    def threshold_array(self):
-        return self["value"] * self["stdev"]
+    def _threshold_array(self):
+        return self._value * self._statistics["stdev"]
 
-    def _gaussian_threshold(self):
-        return self["value"] *self["stdev"] - self["mean"]
+    def _gaussian_pdf_value(self):
+        return self._value * self._statistics["stdev"] - self._statistics["mean"]
 
 
 class StdevAnomalyEvent(StdevEvent):
 
-    def __init__(self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics):
+    def __init__(
+        self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics
+    ):
         # NB: the granny Event has still is_anomaly=False!
         super().__init__(value=value, operator=operator, **statistics)
 
-    def threshold_array(self):
-        return self["value"] * self["stdev"] + self["mean"]
+    def _threshold_array(self):
+        return self._value * self._statistics["stdev"] + self._statistics["mean"]
 
-    def _gaussian_threshold(self):
-        return self["value"] + np.full_like(self["stdev"],0.)
+    def _gaussian_pdf_value(self):
+        return self._value + np.full_like(self._statistics["stdev"], 0.0)
 
 
-# TODO
-class PercentileEvent(Event):
-    # TODO: how about tercile, quartile, decile, etc.
+class _QuantileEvent(_Event):
 
-    def __init__(self, value: float, operator: Literal[">", ">=", "<", "<="], **statistics):
-        super().__init__(value=value, operator=operator, type="percentile", is_anomaly=False, **statistics)
+    def __init__(
+        self,
+        value: int,
+        type: QUANTILE_TYPES,
+        operator: Literal[">", ">=", "<", "<="],
+        **statistics,
+    ):
+        super().__init__(
+            value=value,
+            operator=operator,
+            type=type,
+            is_anomaly=False,
+            **statistics,
+        )
 
-    def compute(self, array, percentile):
-        binary = self.operator()(array, percentile)
+    def _threshold_array(self):
+        return self._statistics["quantiles"][self._value]
 
+    def _gaussian_pdf_value(self):
+        return (
+            self._statistics["quantiles"][self._value] - self._statistics["mean"]
+        ) / self._statistics["stdev"]
+
+
+class PercentileEvent(_QuantileEvent):
+
+    ncats = 100
+
+    def __init__(
+        self, value: int, operator: Literal[">", ">=", "<", "<="], **statistics
+    ):
+        super().__init__(
+            value=value,
+            operator=operator,
+            type="percentile",
+            **statistics,
+        )
+
+
+class TercileEvent(_QuantileEvent):
+
+    ncats = 3
+
+    def __init__(
+        self, value: int, operator: Literal[">", ">=", "<", "<="], **statistics
+    ):
+        super().__init__(
+            value=value,
+            operator=operator,
+            type="tercile",
+            **statistics,
+        )
 
 
 ###############################################################################################
+
 
 def brier_score(probability, event_occurrence):
     """Compute local Brier score.
@@ -219,19 +242,19 @@ def brier_score(probability, event_occurrence):
     Returns:
 
     """
-    return ((probability - event_occurrence) ** 2)
+    return (probability - event_occurrence) ** 2
 
 
 def brier_gaussian(event, array):
-    """Compute Brier score of a forecast described by the Gaussian PDF.
-    """
+    """Compute Brier score of a forecast described by the Gaussian PDF."""
     event_occurance = event.event_array(array)
     prob_array = event.probability_gaussian()
     return brier_score(event_occurance, prob_array)
 
+
 # TODO: not yet tested
 def contingency_table(forecast_occurrence, observation_occurrence):
-    # TODO: implement these two options
+    # TODO: implement other options
     # if method == "members":
     #     nens = len(e)
     #     rnk0 = sum(e)
@@ -241,21 +264,26 @@ def contingency_table(forecast_occurrence, observation_occurrence):
     nens, ngp = forecast_occurrence.shape
     rnk0 = np.nansum(forecast_occurrence, axis=1)
     # construct indices (rounding to cater for potential grib packing)
-    ind_rnk=np.nanmin(np.nanmax(rnk0.round().astype(int),0),nens)
-    where_observed = np.isclose(observation_occurrence[0],1.0)
+    ind_rnk = np.nanmin(np.nanmax(rnk0.round().astype(int), 0), nens)
+    where_observed = np.isclose(observation_occurrence[0], 1.0)
     # set contingency table
     ct = np.zeros((2 * (nens + 1), ngp), float)
-    cocc = ct[slice(0, nens + 1),:]
-    cnoc = ct[slice(nens + 1, None),:]
+    cocc = ct[slice(0, nens + 1), :]
+    cnoc = ct[slice(nens + 1, None), :]
     cocc[ind_rnk[where_observed], where_observed] = 1.0
     cnoc[ind_rnk[~where_observed], ~where_observed] = 1.0
     # TODO: apply joined nan mask of input arrays
     return cocc, cnoc
 
-if __name__=="__main__":
-    mean = np.array([3.9,4.,4.1,4.])
-    stdev = np.array([0.2,0.3,0.3,0.3])
-    event = AbsAnomalyEvent(.2,">", mean=mean, stdev=stdev)
-    observations = np.array([3.3,3.8,4.2,4.])
+
+if __name__ == "__main__":
+    mean = np.array([3.9, 4.0, 4.1, 4.0])
+    stdev = np.array([0.2, 0.3, 0.3, 0.3])
+    perc_60 = np.array([4.0, 4.2, 4.4, 4.8])
+    event = AbsAnomalyEvent(0.2, ">", mean=mean, stdev=stdev)
+    observations = np.array([3.3, 3.8, 4.2, 4.0])
     bsgaus = brier_gaussian(event, observations)
-    print(bsgaus)
+    print(event, bsgaus)
+    event = PercentileEvent(60, ">", mean=mean, stdev=stdev, quantiles={60: perc_60})
+    bsgaus = brier_gaussian(event, observations)
+    print(event, bsgaus)
