3c3e8b6040741c4b5b384002a37217b06119ad7c
oisin-m <oisinmmm@gmail.com>
2026-01-29 11:53:49 +0100

    feat: add quantile score function

--- src/earthkit/meteo/score/ensemble.py
+++ src/earthkit/meteo/score/ensemble.py
@@ -35,13 +35,13 @@ def spread(fcst: T, over: str | list[str], reference: T | None = None) -> T:
 
     .. math::
 
-        e_i = f_i - o_i
+        s_i = \sqrt{ \frac{1}{N} \sum_{i=1}^N \left(f_i - r\right)^2}
 
     where:
 
     - :math:`f_i` is the forecast,
-    - :math:`o_i` are the observations,
-    - :math:`e_i` is the error.
+    - :math:`r` is the reference,
+    - :math:`s_i` is the spread.
 
     Parameters
     ----------
@@ -65,3 +65,53 @@ def spread(fcst: T, over: str | list[str], reference: T | None = None) -> T:
         if over in reference.dims:
             reference = reference.squeeze(over)
     return ((fcst - reference) ** 2).mean(dim=over).sqrt()
+
+
+def quantile_score(fcst: T, obs: T, tau: float, over: str | list[str]) -> T:
+    r"""
+    Calculates the quantile score of a forecast compared to a observations.
+
+    The quantile score is defined as:
+
+    .. math::
+        nowrap::
+
+        \begin{align*}
+        q_i & = \text{Quantile of the forecast at level } \tau \\
+        qs_i = |o_i - q_i| + (2 \tau - 1) (o_i - q_i)
+        \end{align*}
+
+    where:
+
+    - :math:`f_i` is the forecast,
+    - :math:`o_i` are the observations,
+    - :math:`\tau` is the quantile level,
+    - :math:`qs_i` is the quantile score.
+
+    Parameters
+    ----------
+    fcst : xarray object
+        The forecast xarray.
+    obs : xarray object
+        The observations xarray.
+    tau : float
+        The quantile level (between 0 and 1).
+    over : str or list of str
+        The dimension(s) over which to compute the quantile score.
+
+    Returns
+    -------
+    xarray object
+        The quantile score of the forecast compared to the observations.
+    """
+    qf = fcst.quantile(tau, dim=over)
+    # qf = numpy.nanpercentile(e, tau * 100., axis=0)
+    qscore = abs(obs - qf) + (2.0 * tau - 1.0) * (obs - qf)
+    return qscore
+
+
+# def crps_from_ensemble
+
+# def crps_from_gaussian
+
+# def crps_from_cdf
