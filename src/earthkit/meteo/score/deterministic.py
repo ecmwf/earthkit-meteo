960057e6e2a82eb4f9f3886cb3674b8b58c864b1
Andreas Grafberger <18516896+andreas-grafberger@users.noreply.github.com>
2026-01-29 10:50:48 +0100

    copy signatures of deterministic metrics into new file

--- src/earthkit/meteo/score/deterministic.py
+++ src/earthkit/meteo/score/deterministic.py
@@ -10,3 +10,91 @@ standard_deviation_of_error
 correlation
 kge
 """
+
+from typing import Literal
+from typing import TypeVar
+
+import xarray as xr
+
+T = TypeVar("T", xr.DataArray, xr.Dataset)
+
+
+def error(
+    fcst: T,
+    obs: T,
+    agg_method: Literal["mean"] | None = None,
+    agg_dim: str | list[str] | None = None,
+    agg_weights: xr.DataArray | None = None,
+) -> T:
+    pass
+
+
+def mean_error(
+    fcst: T,
+    obs: T,
+    agg_dim: str | list[str] | None = None,
+    agg_weights: xr.DataArray | None = None,
+) -> T:
+    pass
+
+
+def abs_error(
+    fcst: T,
+    obs: T,
+    agg_method: Literal["mean"] | None = None,
+    agg_dim: str | list[str] | None = None,
+    agg_weights: xr.DataArray | None = None,
+    is_angular: bool = False,
+) -> T:
+    pass
+
+
+def mean_abs_error(
+    fcst: T,
+    obs: T,
+    over: str | list[str],
+    weights: xr.DataArray | None = None,
+    is_angular: bool = False,
+) -> T:
+    pass
+
+
+def squared_error(
+    fcst: T,
+    obs: T,
+    agg_method: Literal["mean"] | None = None,
+    agg_dim: str | list[str] | None = None,
+    agg_weights: xr.DataArray | None = None,
+    is_angular: bool = False,
+) -> T:
+    pass
+
+
+def mean_squared_error(
+    fcst: T,
+    obs: T,
+    over: str | list[str],
+    weights: xr.DataArray | None = None,
+    is_angular: bool = False,
+) -> T:
+    pass
+
+
+def root_mean_squared_error(
+    fcst: T,
+    obs: T,
+    over: str | list[str],
+    weights: xr.DataArray | None = None,
+    is_angular: bool = False,
+) -> T:
+    pass
+
+
+def standard_deviation_of_error(
+    fcst: T,
+    obs: T,
+    over: str | list[str],
+    weights: xr.DataArray | None = None,
+    is_angular: bool = False,
+) -> T:
+    pass
