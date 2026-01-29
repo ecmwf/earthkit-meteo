b349e91bb81269cadb449afc6498894b7595cfc7
oisin-m <oisinmmm@gmail.com>
2026-01-29 11:48:21 +0100

    feat: add spread function

--- src/earthkit/meteo/score/ensemble.py
+++ src/earthkit/meteo/score/ensemble.py
@@ -8,3 +8,60 @@ crps_from_cdf
 
 continuous_ignorance (maybe)
 """
+
+from typing import TypeVar
+
+import xarray as xr
+
+T = TypeVar("T", xr.DataArray, xr.Dataset)
+
+
+def import_scores_or_prompt_install():
+    try:
+        import scores
+    except ImportError:
+        raise ImportError(
+            "The 'earthkit-meteo[score]' extra is required to use scoring functions. "
+            "Please install it using 'pip install earthkit-meteo[score]'"
+        )
+    return scores
+
+
+def spread(fcst: T, over: str | list[str], reference: T | None = None) -> T:
+    r"""
+    Calculates the spread of a forecast compared to a reference.
+
+    The spread is defined as:
+
+    .. math::
+
+        e_i = f_i - o_i
+
+    where:
+
+    - :math:`f_i` is the forecast,
+    - :math:`o_i` are the observations,
+    - :math:`e_i` is the error.
+
+    Parameters
+    ----------
+    fcst : xarray object
+        The forecast xarray.
+    over : str or list of str
+        The dimension(s) over which to compute the spread.
+    reference : xarray object, optional
+        The reference xarray to compare against. If not provided, the mean of the forecastover `over` is used.
+
+    Returns
+    -------
+    xarray object
+        The spread of the forecast compared to the reference.
+    """
+
+    # TODO: this could call the rmse function
+    if reference is None:
+        reference = fcst.mean(dim=over)
+    else:
+        if over in reference.dims:
+            reference = reference.squeeze(over)
+    return ((fcst - reference) ** 2).mean(dim=over).sqrt()
