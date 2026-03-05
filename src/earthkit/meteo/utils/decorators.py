67a3ffaac70c7f8763229c928befdde54776d5e9
oisin-m <oisinmmm@gmail.com>
2026-03-05 09:29:09 +0100

    feat: precompute sig and param name

--- src/earthkit/meteo/utils/decorators.py
+++ src/earthkit/meteo/utils/decorators.py
@@ -99,7 +99,7 @@ _DISPATCHERS = [XArrayDispatcher(), FieldListDispatcher(), ArrayDispatcher()]
 
 
 def dispatch(func, match=0, xarray=True, fieldlist=True, array=False):
-    """ "
+    """
     Decorator to dispatch function calls based on input data types.
     The dispatch will attempt to route the call to the appropriate implementation based on the type of the specified argument.
     The implementations are assumed to live in submodules named after the data type (e.g., .xarray, .fieldlist, .array) with the same function name as the toplevel function.
@@ -130,15 +130,17 @@ def dispatch(func, match=0, xarray=True, fieldlist=True, array=False):
     if array:
         DISPATCHERS.append(_DISPATCHERS[2])
 
+    sig = signature(func)
+
+    if isinstance(match, int):
+        param_name = list(sig.parameters)[match]
+    else:
+        param_name = match
+
     @wraps(func)
     def wrapper(*args, **kwargs):
-        sig = signature(func)
         bound_args = sig.bind(*args, **kwargs)
         bound_args.apply_defaults()
-        if isinstance(match, int):
-            param_name = list(sig.parameters)[match]
-        else:
-            param_name = match
 
         obj_to_check = bound_args.arguments[param_name]
 
