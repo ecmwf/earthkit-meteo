647b548fdfebc0f893dcf26b6506a0a804fb9014
oisin-m <oisinmmm@gmail.com>
2026-07-30 15:51:15 +0200

    feat: remove existing headers

--- docs/index.rst
+++ docs/index.rst
@@ -1,4 +1,3 @@
-
 earthkit-meteo
 ==============
 
--- src/earthkit/meteo/stats/xarray/__init__.py
+++ src/earthkit/meteo/stats/xarray/__init__.py
@@ -1,11 +1,3 @@
-# (C) Copyright 2026- ECMWF and individual contributors.
-
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-# In applying this licence, ECMWF does not waive the privileges and immunities
-# granted to it by virtue of its status as an intergovernmental organisation nor
-# does it submit to any jurisdiction.
-
 from .extreme_values import fit_gumbel, return_period_to_value, value_to_return_period
 from .numpy_extended import nanaverage
 
